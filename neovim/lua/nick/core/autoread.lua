244d5b07098d248141f9936b1fb812bda6f285df
Xiuyu Li <nickleefly@gmail.com>
2023-03-14 17:18:31 +0800

    add autoread

---
+++ neovim/lua/nick/core/autoread.lua
@@ -0,0 +1,12 @@
+local w = vim.loop.new_fs_poll()
+local function on_change(bufNr, filePath)
+  vim.api.nvim_command('checktime ' .. bufNr)
+  w:stop()
+  watch_file(bufNr, filePath)
+end
+function watch_file(bufNr, filePath)
+  w:start(filePath, 1000, vim.schedule_wrap(function(...)
+    on_change(bufNr, filePath) end))
+end
+vim.api.nvim_command(
+  "command! -nargs=0 Watch call luaeval('watch_file(_A[1], _A[2])', [bufnr('%'), expand('%:p')])")
