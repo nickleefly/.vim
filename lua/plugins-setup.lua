645ab9fb9bf16d41dee4d4286bb145e49f28e33a
Xiuyu Li <nickleefly@gmail.com>
2023-07-12 10:26:07 +0800

    nvimtree toggle

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -45,9 +45,6 @@ return lazy.setup({
   -- file explorer
   {
     "nvim-tree/nvim-tree.lua",
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
-    },
     config = function()
       require("nvim-tree").setup()
     end,
