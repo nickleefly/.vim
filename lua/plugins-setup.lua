e6d3302fff92f3f9e61eda9f6735db83b065787b
Xiuyu Li <nickleefly@gmail.com>
2024-01-30 17:22:00 +0800

    update blankline

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -108,6 +108,9 @@ return lazy.setup({
     "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    config = function()
+      require("ibl").setup({})
+    end,
     opts = {
       char = "┊",
       show_trailing_blankline_indent = false,
